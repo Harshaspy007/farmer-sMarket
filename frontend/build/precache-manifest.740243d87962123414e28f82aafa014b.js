8e723ecf2033fae30c09a11fe426e77a3d955e35
Harshaspy007 <harshaspy012@gmail.com>
2021-07-02 14:25:11 +0530

    02-07-2021

---
+++ frontend/build/precache-manifest.740243d87962123414e28f82aafa014b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c2b9d7ac4a579d7978806dc068a7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85ebaf09884136c5089",
+    "url": "/static/css/main.a27dc206.chunk.css"
+  },
+  {
+    "revision": "e8f4e9ca212a00f538d8",
+    "url": "/static/js/2.3880d0a3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3880d0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85ebaf09884136c5089",
+    "url": "/static/js/main.46ab1b00.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
